678028fbd943b02f77e86e35e7465ed6b8b5cda7
meldi-akelax <76161523+meldi-akelax@users.noreply.github.com>
2024-09-02 16:26:28 +0100

    Update settings.py

--- laxtech_core/settings.py
+++ laxtech_core/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'laxtech_db',
         'USER': 'postgres',
-        'PASSWORD': 'derferger',
+        'PASSWORD': '2024$Laxtech',
         'HOST': '127.0.0.1',
        'PORT': '5432',
     }    
