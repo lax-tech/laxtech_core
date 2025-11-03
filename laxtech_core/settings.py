652c8262133f16b65dfef848122e4ede0e1fa362
robert akany <robertakany2@gmail>
2025-11-03 11:25:09 +0100

    upgrade

--- laxtech_core/settings.py
+++ laxtech_core/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR.joinpath('static/')]
-STATIC_ROOT = BASE_DIR.joinpath('/static/')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.joinpath('media/')
